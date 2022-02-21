b6bf1bb92976fa60a30bd6876e2f299aa1f2d352
Chris <cangalet@gmail.com>
2022-02-20 22:02:46 -0800

    connect to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const routes = require('./controllers');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
@@ -32,7 +33,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('./controllers/'));
+// turn on routes
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
